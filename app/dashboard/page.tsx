b905a2e243ac9fbaac35c90df79ae4f7128561ee
Jikky <jjikky3@gmail.com>
2026-03-17 18:23:44 +0700

    Delete unnecesarry UI

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,8 +8,7 @@ import { getSite } from '../actions/site';
 export default async function DashboardPage() {
     const initialData = await getSite();
     return (
-        <main className="p-8">
-            <h1 className="text-2xl font-bold mb-4">My Items</h1>
+        <main className="">
             {/* Pass the server data to the Client Component */}
             <Dashboard initialData={initialData? initialData : [] } getSiteFunction={getSite} />
         </main>
